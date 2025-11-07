2abd42b5abb705a0b30d2723e3183905c4b050c5
shivvor2 <jzloo11111@gmail.com>
2025-11-07 20:01:07 +0800

    Fix: fixed github actions error
    Fix linting errors and used properly installed version of package instead of raw source tree

--- denmune_skl/__init__.py
+++ denmune_skl/__init__.py
@@ -1,8 +1,8 @@
 # Authors: scikit-learn-contrib developers
 # License: BSD 3 clause
 
-from .denmune import DenMune
 from ._version import __version__
+from .denmune import DenMune
 
 __all__ = [
     "DenMune",
