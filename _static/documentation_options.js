81098708435374d803f98354759215ce10c39a99
shivvor2 <shivvor2@users.noreply.github.com>
2026-08-21 23:30:20 +0000

    [ci skip] Merge pull request #19 from scikit-learn-contrib/dependabot/github_actions/actions-a949d96a94
    Chore(deps): Bump prefix-dev/setup-pixi from 0.9.6 to 0.10.1 in the actions group across 1 directory b06d026fb657e923fe31631d45252172e2b1b542

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.5.dev6+gb60fe8aba.d20260710',
+    VERSION: '0.1.5.dev8+gb06d026fb.d20260821',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
